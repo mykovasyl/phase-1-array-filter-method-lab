be4c0d514beaaaa61304486a673098f3dde62888
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-01-27 10:37:37 -0500

    commit tree c3bb7a5cb455eb53fc85e6c7548a107bd389895d

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(someArray, driverName) {
+    return someArray.filter(drvr => drvr.toLowerCase() === driverName.toLowerCase());
+}
+
+function fuzzyMatch(someArray, firstLetters) {
+    return someArray.filter(name => name.startsWith(firstLetters))
+}
+
+function matchName(someArray, driversName) {
+    return someArray.filter(drvrName => drvrName.name === driversName)
+}
